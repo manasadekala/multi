8e31d66876e845907ee368893cdf64f59595fae6
manasa <manasadekala19@email.com>
2024-03-26 14:52:01 +0530

    multiple

---
+++ Multi.java
@@ -0,0 +1,10 @@
+public class Multi
+{
+   public static void main(String[]arus)
+    {
+        int x=5;
+        int y=4;
+        int multiplication=x*y;
+        System.out.println(multiplication);
+    }
+}
