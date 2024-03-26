39afcb8a01e31ce94ede44a4ccf0675b543520b4
manasa <manasadekala19@email.com>
2024-03-26 15:05:06 +0530

    multip

--- Multi.java
+++ Multi.java
@@ -2,7 +2,7 @@ public class Multi
 {
    public static void main(String[]arus)
     {
-        int x=5;
+        int x=4;
         int y=4;
         int multiplication=x*y;
         System.out.println(multiplication);
