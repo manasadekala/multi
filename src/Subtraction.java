abbf8c01b5efaea9f085b97edac5c2c99812dfce
manasa <manasadekala19@email.com>
2024-03-26 15:35:13 +0530

    subtarcion

---
+++ src/Subtraction.java
@@ -0,0 +1,8 @@
+public class Subtraction {
+    public static void main(String[]arus){
+        int x=6;
+        int y=5;
+        int subtracion=x-y;
+        System.out.println(subtracion);
+    }
+}
